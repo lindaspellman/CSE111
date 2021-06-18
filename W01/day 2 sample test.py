e3c92917d8a26fa2fdb8ae40b5f2d631184d324e
Linda Spellman <lindaspellman@live.com>
2021-06-18 12:57:47 -0600

    Adding 2 CSV files and receipt file

--- W01/day 2 sample test.py
+++ W01/day 2 sample test.py
@@ -5,4 +5,4 @@ name = input("Hello! What is your name? ")
 for number in reversed(range(1, 4)):
     print(number)
 
-print(f"Welcome to CSE 111, {name}!") 
+print(f"Welcome to CSE 111, {name}!!!") 
