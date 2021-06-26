6b92f89a942ff7aaa6606a6ef60cefa1afde42ff
Linda Spellman <lindaspellman@live.com>
2021-06-26 13:53:34 -0600

    finished receipt.py

--- W09/New folder/receipt.py
+++ W09/New folder/receipt.py
@@ -3,13 +3,34 @@ request.csv file in order to create dictionaries. ???"""
 
 import csv
 
+# Import the datatime module so that
+# it can be used in this program.
+from datetime import datetime
+
 def main():
+    try:
+        print("Inkom Emporium\n")
+        # dictionary = read_products(r"C:\Users\Linda\OneDrive\Desktop\Python Programming\CSE111 2021 Programming with Functions\W09\New folder\products.csv")
+        dictionary = read_products("products.csv")
 
-    # dictionary = read_products(r"C:\Users\Linda\OneDrive\Desktop\Python Programming\CSE111 2021 Programming with Functions\W09\New folder\products.csv")
-    dictionary = read_products("products.csv")
+        # process_request(r"C:\Users\Linda\OneDrive\Desktop\Python Programming\CSE111 2021 Programming with Functions\W09\New folder\request.csv", dictionary)
+        process_request("request.csv", dictionary)
 
-    # process_request(r"C:\Users\Linda\OneDrive\Desktop\Python Programming\CSE111 2021 Programming with Functions\W09\New folder\request.csv", dictionary)
-    process_request("request.csv", dictionary)
+        # Call the now() method to get the current date and
+        # time as a datetime object from the computer's clock.
+        current_date_and_time = datetime.now()
+        print() 
+        # Format and print the current date and time to include
+        # only the day of the week, the hour, and the minute.
+        print("Thank you for shopping at the Inkom Emporium.")
+        print(f"{current_date_and_time:%a %b %d %I:%M:%S %Y}")
+    
+    except (FileNotFoundError, PermissionError) as ex:
+        print(type(ex).__name__, ex, sep=": ")
+        print("Please choose a different file.")
+    except KeyError as ex:
+        print(type(ex).__name__, ex, sep=": ")
+        print(f"That is not a valid key.")
 
 def read_products(filename):
 
@@ -23,10 +44,10 @@ def read_products(filename):
         for rows in reader:
             dictionary[rows[0]] = [rows[1], rows[2]]
 
-    print("\nProducts")
+    # print("\nProducts")
 
-    for key in dictionary:
-        print(key + " " + str(dictionary[key]))
+    # for key in dictionary:
+    #     print(key + " " + str(dictionary[key]))
 
     # Return the dictionary.
     return dictionary
@@ -38,14 +59,26 @@ def process_request(filename, dictionary):
 
         next(reader)
 
-        print("\nRequested Items")
+        # print("\nRequested Items")
+        num_items = 0
+        subtotal = 0
+        total = 0
 
         for rows in reader:
             key = rows[0]
             keyList = dictionary[key]
-            amountOrdered = rows[1]
+            amountOrdered = int(rows[1])
             productName = keyList[0]
-            productPrice = keyList[1]
+            productPrice = float(keyList[1])
             print(f"{productName}: {amountOrdered} @ {productPrice}")
+            subtotal += amountOrdered * productPrice
+            num_items += amountOrdered
+        
+        print(f"\nNumber of items: {num_items}")
+        print(f"Subtotal: {subtotal}")
+        sales_tax = round(subtotal * 0.06, 2)
+        print(f"Sales Tax: {sales_tax}")
+        total = subtotal + sales_tax 
+        print(f"Total: {total}")
 
 main()
